976f5aed78514de421c7f317a5c34882d7754d42
Joel Pfeiffer <joelpf@microsoft.com>
2019-03-14 11:35:10 -0700

    Adding validation and logging

--- mpfilereader.py
+++ mpfilereader.py
@@ -3,15 +3,15 @@ from multiprocessing.pool import ThreadPool
 import numpy as np
 import os
 import time
+import logging
 
 
 def GetChunk(dataloc, filename, start, end):
-    print('Beginning', start, end)
+    logging.debug(f'Beginning {start} to {end}')
     with open(filename, 'rb') as fin:
         fin.seek(start)
-        print('Waiting', start, end)
         dataloc[start:end] = np.fromfile(fin, dtype=np.int8, count=end-start)
-    print('Ending', start, end)
+    logging.debug(f'Endings {start} to {end}')
     return start,end
 
 
@@ -45,11 +45,16 @@ if __name__ == '__main__':
     PARSER.add_argument('-p', '--processes', default=8, type=int, help='Number of processes to use')
     PARSER.add_argument('-c', '--chunksize', default=1000000, type=int, help='Size of chunks to read')
     PARSER.add_argument('-a', '--cap', type=int, default=None, help='Cap the filesize (test)')
+    PARSER.add_argument('-v', '--validate', action='store_true', help='validatet the files are identical')
+    PARSER.set_defaults(validate=False)
     ARGS = PARSER.parse_args()
+    logging.basicConfig(level=logging.DEBUG)
 
     start = time.time()
     dloc = MPFileReader(ARGS.datafile, ARGS.processes, ARGS.chunksize, ARGS.cap)
-    print(time.time() - start)
+    logging.info(f'Time was {time.time() - start}')
 
-    print(dloc[0:20].tobytes())
-    print(open(ARGS.datafile, 'rb').read(20))
+    if ARGS.validate:
+        dloc2 = open(ARGS.datafile, 'rb').read()
+        logging.info(f'Validation: {all(a == b for a, b in zip(dloc.tobytes(), dloc2))}')
+    
